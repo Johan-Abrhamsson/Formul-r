b00acd8cd1d4a94320daa8b2237395bc8623d7a6
Johan Abrahamsson <71386129+Johan-Abrhamsson@users.noreply.github.com>
2020-12-17 15:53:38 +0100

    Tec done!

--- javascrips.js
+++ javascrips.js
@@ -1,25 +1,91 @@
 var HumanPoints = 0;
 var YahooAwnsersPoints = 0;
 var ArtIntPoints = 0;
-
+var whatYouAre = "Unknown";
+var done=0;
 function See() {
-  var awnser = document.from[0];
-  var point = "";
-  for (var i = 1; i < awnser.length; i++) {
-    if (awnser[i].checked) {
-     point = awnser[i].value;
-     if (point=="Human"){
-         HumanPoints+=1
-     }
-     else if (point=="Yahoo"){
-         YahooAwnsersPoints+=1
-     }
-     else if (point=="Artint"){
-         ArtIntPoints+=1
-     }
-    }
+  var form = document.querySelector("form");
+  var nameValue = "A1";
+  var numberOfQuestions = document.querySelectorAll("#quiz-questions .question")
+    .length;
+  for (var i = 0; i<5; i++){
+  switch (form.elements[nameValue].value){
+  case "Human":{
+    HumanPoints += 1;
+    break;
   }
+  case "Yahoo":{
+    YahooAwnsersPoints += 1;
+    break;
+  }
+  case "ArtInt":{
+    ArtIntPoints += 1;
+    break;
+  }
+  default:{
+    break;
+  }
+  }
+  switch (i){
+  case 0:
+  nameValue = "A2";
+  break;
+  case 1:
+  nameValue = "A3";
+  break;
+  case 2:
+  nameValue = "A4";
+  break;
+  case 3:
+  nameValue = "A5";
+  break;
+  case 4:
+  break;
+  }
+}
+
+//document.getElementById("namefirst").value=name;
+
+if (HumanPoints>YahooAwnsersPoints && HumanPoints>ArtIntPoints){
+  whatYouAre = "Human!";
+  document.getElementById("whatYouAreHtml").value = whatYouAre;
+}
+else if (YahooAwnsersPoints>HumanPoints && YahooAwnsersPoints>ArtIntPoints){
+  whatYouAre = "Yahoo Awnsers!";
+  document.getElementById("whatYouAreHtml").value = whatYouAre;
+}
+else if (ArtIntPoints>HumanPoints && ArtIntPoints>YahooAwnsersPoints){
+  whatYouAre = "Artificial Intelligence!";
+  document.getElementById("whatYouAreHtml").value = whatYouAre;
+}
+else if (HumanPoints==ArtIntPoints){
+  whatYouAre = "Artificial Human!"
+  document.getElementById("whatYouAreHtml").value = whatYouAre;
+}
+else if (HumanPoints==YahooAwnsersPoints){
+whatYouAre = "Yahoo Human!"
+document.getElementById("whatYouAreHtml").value = whatYouAre;
+}
+else if (ArtIntPoints==YahooAwnsersPoints){
+  whatYouAre = "Yahoo Intelligece!"
+  document.getElementById("whatYouAreHtml").value = whatYouAre;
+}
+else {
+  whatYouAre = "I dont know";
+  document.getElementById("whatYouAreHtml").value = whatYouAre;
+}
+if (!done==1){
+document.getElementById("fHuman").value = HumanPoints * 20 + "%";
+document.getElementById("fYahoo").value = YahooAwnsersPoints * 20 + "%";
+document.getElementById("fArtInt").value = ArtIntPoints * 20 + "%";
+  console.log(form.elements["A1"].value);
+  console.log(form.elements["A2"].value);
+  console.log(form.elements["A3"].value);
+  console.log(form.elements["A4"].value);
+  console.log(form.elements["A5"].value);
   console.log(HumanPoints);
-  console.log(YahooAwnserPoints);
+  console.log(YahooAwnsersPoints);
   console.log(ArtIntPoints);
+  done++;
+}
 }
